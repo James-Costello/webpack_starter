d4aafde00b44aec115ad31414ecf813ea7681a4a
James Costello <jcostello10@gmail.com>
2017-03-20 20:05:55 -0700

    initializing repo

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: '/dist',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader'],
+            publicPath: '/dist'
+      })
+      }
+    ]
+  },
+    devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+   plugins: [
+     new HtmlWebpackPlugin({
+      title: 'Project',
+      minify: {
+        collapseWhitespace: true
+      },
+      template: './src/index.ejs',
+    }),
+     new ExtractTextPlugin({
+      filename: 'app.css',
+      // disabled: false,
+      allChunks: true
+     }),
+  ]
+}
