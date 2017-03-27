5cca25d0d6762486f85989ce25d7873e6d18cadc
James Costello <jcostello10@gmail.com>
2017-03-27 15:50:20 -0700

    react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,12 +1,98 @@
 const css = require('./app.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Router, Route, IndexRoute, hashHistory} from "react-router";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import Layout from "./components/Layout";
 
-// console.log('HELLO from app.js and webpack dev server');
 
+console.log('HELLO from app.js and webpack dev server');
+
+const app = document.getElementById('root')
 
 ReactDOM.render(
-  <Layout />,
-  document.getElementById('root')
-);
+  <Router>
+    <div>
+      <Route exact path="/" component={Layout}>
+      </Route>
+    </div>
+  </Router>,
+app);
+
+
+// import React from 'react'
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link
+// } from 'react-router-dom'
+
+// const BasicExample = () => (
+//   <Router>
+//     <div>
+//       <ul>
+//         <li><Link to="/">Home</Link></li>
+//         <li><Link to="/about">About</Link></li>
+//         <li><Link to="/topics">Topics</Link></li>
+//       </ul>
+
+//       <hr/>
+
+      // <Route exact path="/" component={Home}/>
+//       <Route path="/about" component={About}/>
+//       <Route path="/topics" component={Topics}/>
+//     </div>
+//   </Router>
+// )
+
+// const Home = () => (
+//   <div>
+//     <h2>Home</h2>
+//   </div>
+// )
+
+// const About = () => (
+//   <div>
+//     <h2>About</h2>
+//   </div>
+// )
+
+// const Topics = ({ match }) => (
+//   <div>
+//     <h2>Topics</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${match.url}/rendering`}>
+//           Rendering with React
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/components`}>
+//           Components
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/props-v-state`}>
+//           Props v. State
+//         </Link>
+//       </li>
+//     </ul>
+
+//     <Route path={`${match.url}/:topicId`} component={Topic}/>
+//     <Route exact path={match.url} render={() => (
+//       <h3>Please select a topic.</h3>
+//     )}/>
+//   </div>
+// )
+
+// const Topic = ({ match }) => (
+//   <div>
+//     <h3>{match.params.topicId}</h3>
+//   </div>
+// )
+
+// export default BasicExample
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default class Header extends React.Component {
   render(){
     return (
       <div>
         <h1>Header</h1>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Projects</Link></li>
+            <li><Link to="/topics">Marginalia</Link></li>
+            <li><Link to="/topics">Contact</Link></li>
+          </ul>
       </div>
     )
   }
